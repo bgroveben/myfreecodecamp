224aee5a1dcc3a2af2fa58344dc2c631a3eaed44
bgrove1974 <bgroveben@gmail.com>
2015-09-25 01:01:54 -0700

    Waypoint 135

--- Javascript/Basics/dataStructures.js
+++ Javascript/Basics/dataStructures.js
@@ -72,3 +72,86 @@ var lastLetterOfLastName = lastName[lastName.length - 1];
 // You'll learn about functions soon.
 (function(v){return v;})(lastLetterOfLastName);
 console.log(lastLetterOfLastName);
+
+
+var firstName = "Ada";
+var thirdToLastLetterOfFirstName = firstName[firstName.length - 3];
+var lastName = "Lovelace";
+var secondToLastLetterOfLastName = lastName[lastName.length - 2];
+// Only change code above this line.
+// We use this function to show you the value of your variable in your output box.
+// You'll learn about functions soon.
+(function(v){return v;})(secondToLastLetterOfLastName);
+console.log(secondToLastLetterOfLastName);
+
+
+var sum = 10 + 10; //make this equal to 20 by changing the 0 into the appropriate number.
+// Only change code above this line.
+// We use this function to show you the value of your variable in your output box.
+// You'll learn about functions soon.
+(function(z){return 'sum='+z;})(sum);
+console.log(sum);
+
+
+var difference = 45 - 33; //make this equal to 12 by changing the 0 into the appropriate number.
+// Only change code above this line.
+// We use this function to show you the value of your variable in your output box.
+// You'll learn about functions soon.
+(function(z){return 'difference='+z;})(difference);
+console.log(difference);
+
+
+var product = 8 * 10; // Make this equal to 80 by changing the 0 into the appropriate number.
+// Only change code above this line.
+// We use this function to show you the value of your variable in your output box.
+// You'll learn about functions soon.
+(function(z){return 'product='+z;})(product);
+console.log(product);
+
+
+var quotient = 66 / 33; //make this equal to 2 by changing the 0 into the appropriate number.
+// Only change code above this line.
+// We use this function to show you the value of your variable in your output box.
+// You'll learn about functions soon.
+(function(z){return 'quotient='+z;})(quotient);
+console.log(quotient);
+
+
+// var ourDecimal = 5.7;
+// Create a number with a decimal point here called myDecimal
+var myDecimal = 3.14;
+// Only change code above this line.
+// We use this function to show you the value of your variable in your output box.
+// You'll learn about functions soon.
+(function(){if(typeof(myDecimal) !== "undefined"){return myDecimal;}})();
+console.log(myDecimal);
+
+
+var quotient = 4.4 / 2.0; // equals 2.2
+var product = 2.0 * 2.5; // equals 5.0
+// Only change code above this line.
+// We use this function to show you the value of your variable in your output box.
+// You'll learn about functions soon.
+(function(y){return 'product='+y;})(product);
+console.log(quotient);
+console.log(product.toFixed(1));   // JS doesn't have floats and ints, only the Number data type.
+
+
+// var array = ["John", 23];
+// Only change code below this line.
+var myArray = ["Ben", 26];
+// Only change code above this line.
+// We use this function to show you the value of your variable in your output box.
+// You'll learn about functions soon.
+(function(z){return z;})(myArray);
+console.log(myArray);
+
+
+var ourArray = [["the universe", "everything", 42]];
+// Only change code below this line.
+var myArray = [["pi", 3.14]];
+// Only change code above this line.
+// We use this function to show you the value of your variable in your output box.
+// You'll learn about functions soon.
+if(typeof(myArray) !== "undefined"){(function(){return myArray;})();}
+console.log(myArray);
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 Free Code Camp -- 800 hours (or so) of practice.
 
-http://www.freecodecamp.com/challenges/waypoint-use-bracket-notation-to-find-the-nthtolast-character-in-a-string
+http://www.freecodecamp.com/challenges/waypoint-access-array-data-with-indexes
 
 Javascript/Basics/dataStructures.js
