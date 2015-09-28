74d02c47c68df638720a7831307ea7bb75fd38a6
bgrove1974 <bgroveben@gmail.com>
2015-09-28 00:30:20 -0700

    Waypoint 149

--- Javascript/Basics/functionsEtc.js
+++ Javascript/Basics/functionsEtc.js
@@ -113,3 +113,27 @@ function mathFunction() {
 console.log(mathFunction());
 // We use this function to show you the value of your variable in your output box.
 (function(){return mathFunction();})();
+
+
+function randFunction(){
+  // Make myFunction return a random number between zero and nine instead of a decimal.
+  // Only change code below this line.
+  return Math.floor(Math.random() * 10);
+  // Only change code above this line.
+}
+console.log(randFunction());
+// We use this function to show you the value of your variable in your output box.
+(function(){return randFunction();})();
+
+
+var min = 0;
+var max = 9;
+function newRandFunction() {
+  // Make myFunction return a random number between zero and nine instead of a decimal
+  // Only change code below this line.
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+console.log(newRandFunction());
+// Only change code above this line.
+// We use this function to show you the value of your variable in your output box.
+(function(){return newRandFunction();})();
--- README.md
+++ README.md
@@ -2,4 +2,4 @@ Free Code Camp -- 800 hours (or so) of practice.
 
 http://www.freecodecamp.com/challenges/waypoint-generate-random-whole-numbers-with-javascript
 
-Javascript/Basics/dataStructures.js
+Javascript/Basics/FunctionsEtc.js
