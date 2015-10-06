a6b86b2128a2f26146952585073aaa8281649fdf
bgrove1974 <bgroveben@gmail.com>
2015-10-06 02:24:27 -0700

    Waypoint 183

--- Javascript/Algorithms/basic.js
+++ Javascript/Algorithms/basic.js
@@ -189,3 +189,18 @@ function slasher(arr, howMany) {
 }
 console.log(slasher([1, 2, 3], 2, ""));
 console.log(slasher([1, 2, 3], 0, ""));
+
+
+// Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+function mutation(arr) {
+  var first = arr[0].toLowerCase();
+  var second = arr[1].toLowerCase();
+  for (var i=0; i < second.length; i++) {
+    if (first.indexOf(second.charAt(i)) === -1) {
+      return false
+    }
+  }
+  return true;
+}
+console.log(mutation(["hello", "neo"], ""));
+console.log(mutation(["Alien", "line"], ""));
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Free Code Camp -- 800 hours (or so) of practice.
 
-http://www.freecodecamp.com/challenges/bonfire-mutations
+http://www.freecodecamp.com/challenges/bonfire-falsy-bouncer
 
 Basic Algorithm Scripting
 Javascript/Algorithms/basic.js
