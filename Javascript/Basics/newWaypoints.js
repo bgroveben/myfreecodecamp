cc5fbe5c9aa25a02884cbf650a5cc40471601850
bgrove1974 <bgroveben@gmail.com>
2016-01-04 02:04:12 -0800

    Working my way through Waypoints

---
+++ Javascript/Basics/newWaypoints.js
@@ -0,0 +1,39 @@
+// Waypoints starting at about #24 (they have a tendency to change over time)
+
+var myStr = "I am a \"double quoted\" string inside \"double quotes\""; // Change this line
+console.log(myStr);
+
+var myStr = "<a href=\"http://www.example.com\" target=\"_blank\">Link</a>";
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+// Encode the following sequence, separated by spaces:
+// backslash tab tab carriage-return new-line and assign it to myStr
+var myStr = "\\ \t \t \r \n"; // Change this line
+
+// Example
+var ourStr = "I come first. " + "I come second.";
+// Only change code below this line
+var myStr = "This is the start. " + "This is the end.";
+
+// Example
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+// Only change code below this line
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
+
+// Example
+var ourName = "Free Code Camp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+// Only change code below this line
+var myName = "myName";
+var myStr = "My name is " + myName + "And I am swell!";
+
+// Example
+var anAdjective = "awesome!";
+var ourStr = "Free Code Camp is ";
+ourStr += anAdjective;
+// Only change code below this line
+var someAdjective = "fun!";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
--- README.md
+++ README.md
@@ -13,4 +13,4 @@ http://codepen.io/bgroveben/pen/qOXEwZ
 
 Whoa! WaitAMinute! FCC has a bunch of new exercises added, so I'm going to fill in the blanks.
 
-http://www.freecodecamp.com/challenges/waypoint-escaping-literal-quotes-in-strings
+http://www.freecodecamp.com/challenges/waypoint-understand-string-immutability
