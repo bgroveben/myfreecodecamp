8f30dc4f14f53c760bcc245823a2d5361e898485
bgrove1974 <bgroveben@gmail.com>
2016-01-07 05:28:56 -0800

    More javascripting, sucka

--- Javascript/Basics/newCheckpoints.js
+++ Javascript/Basics/newCheckpoints.js
@@ -5,7 +5,6 @@ You will create an (optionally humorous) "Fill in the Blanks" style sentence.
 You will need to use string operators to build a new string, result, using the provided variables:
 myNoun, myAdjective, myVerb, and myAdverb.
 */
-
 function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
     var result = "";
     // Your code below this line
@@ -19,3 +18,12 @@ function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
 }
 // Change the words here to test your function
 console.log(wordBlanks("dog", "big", "ran", "quickly"));
+
+
+/*
+Create a shopping list in the variable myList. The list should be a multi-dimensional array containing several sub-arrays.
+The first element in each sub-array should contain a string with the name of the item. The second element should be a number representing the quantity i.e.
+["Chocolate Bar", 15]
+There should be at least 5 sub-arrays in the list
+*/
+var myList = [["level1", 1], ["level2", 2], ["level3", 3], ["level4", 4], ["level5", 5]];
--- Javascript/Basics/newWaypoints.js
+++ Javascript/Basics/newWaypoints.js
@@ -60,3 +60,29 @@ function myFunction(first, second) {
   console.log(first + second);
 }
 myFunction('my', 'Function');
+
+
+/*Instructions
+Declare a global variable myGlobal outside of any function.
+Initialize it to have a value of 10.
+Inside function fun1, assign 5 to oopsGlobal without using the var keyword.
+*/
+// Declare your variable here
+var myGlobal = 10;
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+  oopsGlobal = 5;
+}
+// Only change code above this line
+function fun2() {
+  var output = "";
+  if(typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if(typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
+console.log(fun1());
+console.log(fun2());
