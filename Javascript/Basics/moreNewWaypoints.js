57b33ad45b23912a25204be5ab7bfc7205b2cec2
bgrove1974 <bgroveben@gmail.com>
2016-01-10 00:05:10 -0800

    Switch statements

--- Javascript/Basics/moreNewWaypoints.js
+++ Javascript/Basics/moreNewWaypoints.js
@@ -128,3 +128,108 @@ function chainedIfElse(number) {
 console.log(chainedIfElse(4));
 console.log(chainedIfElse(14));
 console.log(chainedIfElse(24));
+console.log();
+
+
+/*
+Waypoint: Selecting from many options with Switch Statements
+If you have many options to choose from, use a switch statement.
+A switch statement tests a value and can have many case statements which defines various possible values.
+Statements are executed from the first matched case value until a break is encountered.
+*/
+// Instructions: Write a switch statement which tests val and sets answer for the following conditions:
+function switchTest(val) {
+  var answer = "";
+  switch (val) {
+    case 1:
+      answer = "alpha";
+      break;
+    case 2:
+      answer = "beta";
+      break;
+    case 3:
+      answer = "gamma";
+      break;
+    case 4:
+      answer = "delta";
+      break;
+  }
+  return answer;
+}
+console.log(switchTest(1));
+console.log(switchTest(2));
+console.log(switchTest(3));
+console.log(switchTest(4));
+console.log();
+
+
+/*
+Waypoint: Adding a default option in Switch statements
+Instructions
+Write a switch statement to set answer for the following conditions:
+"a" - "apple"
+"b" - "bird"
+"c" - "cat"
+default - "stuff"
+*/
+function defaultOption(value) {
+  answer = "";
+  switch (value) {
+  case "a":
+    answer = "apple";
+    break;
+  case "b":
+    answer = "bird";
+    break;
+  case "c":
+    answer = "cat";
+    break;
+  default:
+    answer = "stuff";
+  }
+  return answer;
+}
+console.log(defaultOption(1));
+console.log(defaultOption("a"));
+console.log();
+
+
+/*
+Waypoint: Multiple Identical Options in Switch Statements
+If the break statement is ommitted from a switch statement's case, the following case statement(s) are executed
+until a break is encountered.
+If you have multiple inputs with the same output, you can represent them in a switch statement.
+Instructions
+Write a switch statement to set answer for the following ranges:
+1-3 - "Low"
+4-6 - "Mid"
+7-9 - "High"
+Note
+You will need to have a case statement for each number in the range.
+*/
+function multipleOptions(value) {
+  var answer = "";
+  // Only change code below this line
+  switch (value) {
+    case 1:
+    case 2:
+    case 3:
+      answer = "Low";
+      break;
+    case 4:
+    case 5:
+    case 6:
+      answer = "Mid";
+      break;
+    case 7:
+    case 8:
+    case 9:
+      answer = "High";
+      break;
+  }
+  return answer;
+}
+console.log(multipleOptions(2));
+console.log(multipleOptions(5));
+console.log(multipleOptions(8));
+console.log();
--- README.md
+++ README.md
@@ -13,4 +13,4 @@ http://codepen.io/bgroveben/pen/qOXEwZ
 
 Whoa! WaitAMinute! FCC has a bunch of new exercises added, so I'm going to fill in the blanks.
 
-http://www.freecodecamp.com/challenges/waypoint-selecting-from-many-options-with-switch-statements
+http://www.freecodecamp.com/challenges/waypoint-replacing-if-else-chains-with-switch
