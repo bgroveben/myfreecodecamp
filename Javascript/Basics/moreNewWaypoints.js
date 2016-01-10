4f79e8540e57e18926bde2082e6d97e9c80ea31f
bgrove1974 <bgroveben@gmail.com>
2016-01-10 02:04:17 -0800

    Replacing If Else chains with Switch

--- Javascript/Basics/moreNewWaypoints.js
+++ Javascript/Basics/moreNewWaypoints.js
@@ -233,3 +233,48 @@ console.log(multipleOptions(2));
 console.log(multipleOptions(5));
 console.log(multipleOptions(8));
 console.log();
+
+
+/*
+Waypoint: Replacing If Else Chains with Switch
+Instructions: Change the chained if/if else statements into a switch statement.
+*/
+function anIfElse(val) {
+  var answer = "";
+  if(val === "bob") {
+    answer = "Marley";
+  } else if(val === 42) {
+    answer = "The Answer";
+  } else if(val === 1) {
+    answer = "There is no #1";
+  } else if(val === 99) {
+    answer = "Missed me by this much!";
+  } else if(val === 7) {
+    answer = "Ate Nine";
+  }
+  return answer;
+}
+console.log(anIfElse(7));
+
+function aSwitchStatement(val) {
+  var answer = "";
+  switch (val) {
+    case "bob":
+      answer = "Marley";
+      break;
+    case 42:
+      answer = "The Answer";
+      break;
+    case 1:
+      answer = "There is no #1";
+      break;
+    case 99:
+      answer = "Missed me by this much!";
+      break;
+    case 7:
+      answer = "Ate Nine";
+      break;
+  }
+  return answer;
+}
+console.log(aSwitchStatement(7));
