92dab7d009c4960fa368dda137553bf7c57f8509
bgrove1974 <bgroveben@gmail.com>
2016-01-11 01:42:12 -0800

    Checkpoint: Counting Cards

--- Javascript/Basics/newCheckpoints.js
+++ Javascript/Basics/newCheckpoints.js
@@ -82,3 +82,61 @@ console.log(golfScore(5, 4));
 console.log(golfScore(4, 2));
 console.log(golfScore(4, 1));
 console.log();
+
+/*
+Checkpoint: Counting Cards
+In the casino game Blackjack, a player can gain an advantage over the house by keeping track of the relative number of high and low cards remaining in the deck.
+This is called Card Counting.
+Having more high cards remaining in the deck favors the player.
+Each card is assigned a value acountingCardsording to the table below.
+When the count is positive, the player should bet high.
+When the count is zero or negative, the player should bet low.
+Value	Cards
++1 --	2, 3, 4, 5, 6
+0	-- 7, 8, 9
+-1	-- 10, 'J', 'Q', 'K','A'
+You will write a card counting function.
+It will receive a card parameter and increment or decrement the global count variable according to the card's value.
+The function will then return the current count and the string "Bet" if the count is positive, or "Hold" if the count is zero or negative.
+Example Output:
+-3 Hold
+5 Bet
+*/
+var count = 0;
+function countingCards(card) {
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count += 1;
+      break;
+    case 7:
+    case 8:
+    case 9:
+      count += 0;
+      // I used count += 0; on the FcountingCards site because the linter threw a warning -- the results are the same.
+      break;
+    case 10:
+    case 'J':
+    case 'Q':
+    case 'K':
+    case 'A':
+      count -= 1;
+      break;
+  }
+  if (count > 0) {
+    return count + " Bet";
+  } else {
+    return count + " Hold";
+  }
+}
+console.log(countingCards(2));
+console.log(countingCards(3));
+console.log(countingCards(7));
+console.log(countingCards('K'));
+console.log(countingCards('A'));
+console.log(countingCards('Q'));
+console.log(countingCards(2), countingCards(3), countingCards(7), countingCards('K'), countingCards('A'), countingCards('Q'));
+console.log();
--- README.md
+++ README.md
@@ -13,4 +13,4 @@ http://codepen.io/bgroveben/pen/qOXEwZ
 
 Whoa! WaitAMinute! FCC has a bunch of new exercises added, so I'm going to fill in the blanks.
 
-http://www.freecodecamp.com/challenges/waypoint-returning-boolean-values-from-functions
+http://www.freecodecamp.com/challenges/waypoint-accessing-objects-properties-with-the-dot-operator
