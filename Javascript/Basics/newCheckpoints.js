40853e3e657a5dbd70ccbf80868df5ee03ca7ed0
bgrove1974 <bgroveben@gmail.com>
2016-01-13 17:04:20 -0800

    Checkpoint: Record Collection

--- Javascript/Basics/newCheckpoints.js
+++ Javascript/Basics/newCheckpoints.js
@@ -140,3 +140,62 @@ console.log(countingCards('A'));
 console.log(countingCards('Q'));
 console.log(countingCards(2), countingCards(3), countingCards(7), countingCards('K'), countingCards('A'), countingCards('Q'));
 console.log();
+
+
+// Checkpoint: Record Collection
+/*
+You are given a JSON object representing (a small part of) your record collection.
+Each album is identified by a unique id number and has several properties.
+Not all albums have complete information.
+Write a function which takes an id, a property (prop), and a value.
+For the given id in collection:
+If value is non-blank(value !== "") and prop is not "tracks" then update or set the value for the prop.
+If the prop is "tracks" and value is non-blank, push the value onto the end of the tracks array.
+If value is blank, delete that prop.
+Always return the entire collection object.
+*/
+var collection = {
+  2548: {
+    album: "Slippery When Wet",
+    artist: "Bon Jovi",
+    tracks: [
+      "Let It Rock",
+      "You Give Love a Bad Name"
+    ]
+  },
+  2468: {
+    album: "1999",
+    artist: "Prince",
+    tracks: [
+      "1999",
+      "Little Red Corvette"
+    ]
+  },
+  1245: {
+    artist: "Robert Palmer",
+    tracks: [ ]
+  },
+  5439: {
+    album: "ABBA Gold"
+  }
+};
+// Keep a copy of the collection for tests:
+var collectionCopy = JSON.parse(JSON.stringify(collection));
+function update(id, prop, value) {
+  if (prop == "tracks" && value !== "") {
+    collection[id][prop].push(value);
+  } else if (value !== "") {
+    collection[id][prop] = value;
+  } else if (value === "") {
+    delete collection[id][prop];
+  }
+return collection;
+}
+console.log(update(5439, "artist", "ABBA"));
+console.log();
+console.log(update(2548, "artist", ""));
+console.log();
+console.log(update(1245, "tracks", "Addicted to Love"));
+console.log();
+console.log(update(5439, "tracks", ""));
+console.log();
--- README.md
+++ README.md
@@ -13,4 +13,4 @@ http://codepen.io/bgroveben/pen/qOXEwZ
 
 Whoa! WaitAMinute! FCC has a bunch of new exercises added, so I'm going to fill in the blanks.
 
-http://www.freecodecamp.com/challenges/waypoint-accessing-nested-objects-in-json
+http://www.freecodecamp.com/challenges/waypoint-iterate-odd-numbers-with-a-for-loop
